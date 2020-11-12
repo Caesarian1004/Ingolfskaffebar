044672089db250f3600b5bf06c301665ae5ed10c
Caesarian1004 <71384409+Caesarian1004@users.noreply.github.com>
2020-11-12 14:43:30 +0100

    Flyttet fra lokalt

---
+++ main.js
@@ -0,0 +1,14 @@
+let modal=document.getElementById("myModal");
+let submit=document.getElementById("submit");
+let span=document.getElementsByClassName("close")[0];
+submit.onclick=function(){
+    modal.style.display="block";
+}
+span.onclick=function(){
+    modal.style.display="none";
+}
+window.onclick=function(event){
+    if(event.target==modal){
+        modal.style.display="none";
+    }
+}
